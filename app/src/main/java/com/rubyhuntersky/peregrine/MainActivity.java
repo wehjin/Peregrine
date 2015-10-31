ffeaa9b3db7e5c354f304780235427c76211ebb3
Jeffrey Yu <wehjin@gmail.com>
2015-10-31 16:48:06 -0700

    Add Refresh action to menu

--- app/src/main/java/com/rubyhuntersky/peregrine/MainActivity.java
+++ app/src/main/java/com/rubyhuntersky/peregrine/MainActivity.java
@@ -1,13 +1,52 @@
 package com.rubyhuntersky.peregrine;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView netWorthTextView;
+    private TextView refreshTimeTextVIew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        netWorthTextView = (TextView) findViewById(R.id.textview_net_worth);
+        refreshTimeTextVIew = (TextView) findViewById(R.id.textview_refresh_time);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                refreshData();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void refreshData() {
+        netWorthTextView.setText(String.format("$ %s", "Fake data"));
+
+        final String refreshTime = DateFormat.getDateTimeInstance().format(new Date());
+        refreshTimeTextVIew.setText(refreshTime);
     }
 }
