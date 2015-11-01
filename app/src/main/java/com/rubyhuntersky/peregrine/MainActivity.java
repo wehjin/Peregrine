1e967cd1e5c84399a6f3336889c068786cb115aa
Jeffrey Yu <wehjin@gmail.com>
2015-10-31 17:02:44 -0700

    Use Observable to discover worth value

--- app/src/main/java/com/rubyhuntersky/peregrine/MainActivity.java
+++ app/src/main/java/com/rubyhuntersky/peregrine/MainActivity.java
@@ -1,16 +1,25 @@
 package com.rubyhuntersky.peregrine;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.util.Date;
 
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
     private TextView netWorthTextView;
     private TextView refreshTimeTextVIew;
 
@@ -44,9 +53,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void refreshData() {
-        netWorthTextView.setText(String.format("$ %s", "Fake data"));
+        getNetWorth().subscribe(new Action1<BigDecimal>() {
+            @Override
+            public void call(BigDecimal bigDecimal) {
+                netWorthTextView.setText(String.format("$ %s", "Fake data"));
 
-        final String refreshTime = DateFormat.getDateTimeInstance().format(new Date());
-        refreshTimeTextVIew.setText(refreshTime);
+                final String refreshTime = DateFormat.getDateTimeInstance().format(new Date());
+                refreshTimeTextVIew.setText(refreshTime);
+            }
+        }, new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+                Log.e(TAG, "refreshData", throwable);
+                showErrorDialog(throwable);
+            }
+        });
+    }
+
+    private void showErrorDialog(Throwable throwable) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Error")
+               .setMessage(throwable.getMessage())
+               .setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+                       dialog.dismiss();
+                   }
+               })
+               .show();
+    }
+
+    private Observable<BigDecimal> getNetWorth() {
+        return Observable.create(new Observable.OnSubscribe<BigDecimal>() {
+            @Override
+            public void call(Subscriber<? super BigDecimal> subscriber) {
+                subscriber.onNext(null);
+                subscriber.onCompleted();
+            }
+        });
     }
 }
