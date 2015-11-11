c9825253e5e344b050cd289cd68776b2fa69b752
Jeffrey Yu <wehjin@gmail.com>
2015-11-11 04:50:00 -0800

    Add circle to layout

---
+++ app/src/main/java/com/rubyhuntersky/peregrine/CircleView.java
@@ -0,0 +1,51 @@
+package com.rubyhuntersky.peregrine;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author wehjin
+ * @since 11/11/15.
+ */
+
+public class CircleView extends View {
+
+    private Paint paint;
+
+    public CircleView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public CircleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public CircleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    public CircleView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(Context context) {
+        paint = new Paint();
+        paint.setColor(context.getResources().getColor(R.color.colorAccent));
+        paint.setAntiAlias(true);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        final int width = getWidth();
+        final int height = getHeight();
+        final int diameter = Math.min(width, height);
+        canvas.drawCircle(width / 2, height / 2, diameter / 2, paint);
+    }
+}
