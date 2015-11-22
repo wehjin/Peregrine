387e77f17014041a28b3e45f97499fef965db48d
Jeffrey Yu <wehjin@gmail.com>
2015-11-22 11:08:26 -0800

    Add ViewPager and TabLayout

--- app/src/main/java/com/rubyhuntersky/peregrine/BaseFragment.java
+++ app/src/main/java/com/rubyhuntersky/peregrine/BaseFragment.java
@@ -1,10 +1,10 @@
 package com.rubyhuntersky.peregrine;
 
 import android.app.AlertDialog;
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 
 import com.rubyhuntersky.peregrine.exception.NotStoredException;
--- app/src/main/java/com/rubyhuntersky/peregrine/MainActivity.java
+++ app/src/main/java/com/rubyhuntersky/peregrine/MainActivity.java
@@ -1,6 +1,12 @@
 package com.rubyhuntersky.peregrine;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
 public class MainActivity extends BaseActivity {
@@ -9,12 +15,33 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                                .add(R.id.fragmentContainer, new NetValueFragment(), NetValueFragment.TAG)
-                                .commit();
-        }
 
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Fix toolbar text color.  Must follow setSupportActionBar.
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.lightText));
+
+        FragmentPagerAdapter pagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public int getCount() {
+                return 2;
+            }
+
+            @Override
+            public Fragment getItem(int position) {
+                return new NetValueFragment();
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return "Net Value";
+            }
+        };
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(pagerAdapter);
+
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
