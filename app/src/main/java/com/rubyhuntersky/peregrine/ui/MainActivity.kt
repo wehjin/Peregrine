aaefc2c3574f4ea3c29b5cf803da51909569ba1c
Jeffrey Yu <wehjin@gmail.com>
2017-03-08 06:22:28 -0800

    Move MainActivity to Kotlin

---
+++ app/src/main/java/com/rubyhuntersky/peregrine/ui/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.rubyhuntersky.peregrine.ui
+
+import android.os.Bundle
+import android.support.v4.app.FragmentPagerAdapter
+import android.view.Menu
+import android.view.MenuItem
+import com.rubyhuntersky.peregrine.R
+import kotlinx.android.synthetic.main.activity_main.*
+import rx.Subscription
+
+class MainActivity : BaseActivity() {
+
+    private var titleSubscription: Subscription? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTitle(R.string.activity_title)
+
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        viewPager.adapter = object : FragmentPagerAdapter(supportFragmentManager) {
+
+            override fun getCount(): Int {
+                return 3
+            }
+
+            override fun getItem(position: Int): BaseFragment {
+                when (position) {
+                    0 -> return NetValueFragment()
+                    1 -> return AssetsFragment()
+                    2 -> return GroupsFragment()
+                    else -> throw IndexOutOfBoundsException("item position: $position")
+                }
+            }
+
+            override fun getPageTitle(position: Int): CharSequence {
+                when (position) {
+                    0 -> return "Value"
+                    1 -> return "Assets"
+                    2 -> return "Groups"
+                    else -> throw IndexOutOfBoundsException("page-title position: $position")
+                }
+            }
+        }
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        titleSubscription = allAccountsStream.subscribe {
+            toolbar.subtitle = it?.relativeArrivalTime
+        }
+    }
+
+    override fun onPause() {
+        titleSubscription!!.unsubscribe()
+        super.onPause()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_refresh -> {
+                toolbar.setSubtitle(R.string.processing)
+                refresh()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
