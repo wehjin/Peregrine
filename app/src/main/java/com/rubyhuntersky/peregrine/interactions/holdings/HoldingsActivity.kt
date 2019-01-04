7d1d675d44a08b6f3479ba5fc2a05c6ad003b72f
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-01-03 19:25:07 -0800

    Add loading and placeholder rendering to holdings activity.

--- app/src/main/java/com/rubyhuntersky/peregrine/interactions/holdings/HoldingsActivity.kt
+++ app/src/main/java/com/rubyhuntersky/peregrine/interactions/holdings/HoldingsActivity.kt
@@ -1,13 +1,25 @@
 package com.rubyhuntersky.peregrine.interactions.holdings
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.rubyhuntersky.peregrine.R
+import kotlinx.android.synthetic.main.activity_holdings.*
 
 class HoldingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_holdings)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadingTextView.visibility = View.VISIBLE
+        addHoldingFrameLayout.visibility = View.GONE
+        addHoldingButton.setOnClickListener {
+            addHoldingFrameLayout.visibility = View.GONE
+        }
     }
 }
