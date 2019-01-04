79778e2489f0e5ddbded9986a3c6bf3bcb69c9e1
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-01-04 14:47:27 -0800

    Swipe on holding partially enabled

--- app/src/main/java/com/rubyhuntersky/peregrine/interactions/holdings/HoldingsActivity.kt
+++ app/src/main/java/com/rubyhuntersky/peregrine/interactions/holdings/HoldingsActivity.kt
@@ -1,9 +1,11 @@
 package com.rubyhuntersky.peregrine.interactions.holdings
 
+import android.graphics.Canvas
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +13,6 @@ import com.rubyhuntersky.peregrine.R
 import com.rubyhuntersky.peregrine.data.Databook
 import com.rubyhuntersky.peregrine.interactions.newholding.NewHoldingCatalyst
 import kotlinx.android.synthetic.main.activity_holdings.*
-import kotlinx.android.synthetic.main.activity_holdings.view.*
 import kotlinx.android.synthetic.main.listitem_offlineholding.view.*
 import rx.Subscription
 
@@ -22,11 +23,63 @@ class HoldingsActivity : AppCompatActivity() {
     private val reactor = HoldingsReactor(databook, newHoldingCatalyst).apply { start() }
     private var reactorSubscription: Subscription? = null
 
+
+    enum class ItemState {
+        AWAITING_SWIPE,
+        SWIPED,
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_holdings)
         setSupportActionBar(toolbar)
+
         holdingsRecyclerView.layoutManager = LinearLayoutManager(this)
+        ItemTouchHelper(object : ItemTouchHelper.Callback() {
+
+            private val buttonWidth = 300f
+            private var itemState = ItemState.AWAITING_SWIPE
+            private var swipedViewHolder: RecyclerView.ViewHolder? = null
+
+            override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
+                return makeMovementFlags(0, ItemTouchHelper.START)
+            }
+
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+                return false
+            }
+
+            override fun getSwipeThreshold(viewHolder: RecyclerView.ViewHolder): Float = 2.0f
+
+            override fun getSwipeVelocityThreshold(defaultValue: Float): Float = 0f
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {}
+
+            override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE && viewHolder != swipedViewHolder) {
+                    itemState = ItemState.AWAITING_SWIPE
+                    swipedViewHolder?.itemView?.translationX = 0f
+                    swipedViewHolder = null
+                }
+                super.onSelectedChanged(viewHolder, actionState)
+            }
+
+            override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+                when (itemState) {
+                    ItemState.AWAITING_SWIPE -> {
+                        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE && dX < -buttonWidth) {
+                            itemState = ItemState.SWIPED
+                            swipedViewHolder = viewHolder
+                        }
+                        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    }
+                    ItemState.SWIPED -> {
+                        val revisedDx = Math.min(dX, -buttonWidth)
+                        super.onChildDraw(c, recyclerView, viewHolder, revisedDx, dY, actionState, isCurrentlyActive)
+                    }
+                }
+            }
+        }).attachToRecyclerView(holdingsRecyclerView)
     }
 
     override fun onStart() {
