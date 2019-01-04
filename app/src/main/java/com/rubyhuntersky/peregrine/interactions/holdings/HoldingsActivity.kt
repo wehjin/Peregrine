5f4fd69bd408d22fbd6afc0af3123ca1682fc617
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-01-03 18:33:01 -0800

    Added empty Holdings activity

---
+++ app/src/main/java/com/rubyhuntersky/peregrine/interactions/holdings/HoldingsActivity.kt
@@ -0,0 +1,13 @@
+package com.rubyhuntersky.peregrine.interactions.holdings
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.rubyhuntersky.peregrine.R
+
+class HoldingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_holdings)
+    }
+}
